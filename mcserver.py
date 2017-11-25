e1450fdfea37def46a380911a3e90a7dbdf15441
Alan Yan <acyanlight@gmail.com>
2017-11-25 16:17:11 -0500

    finish beginner hw3 - add weekly highcharts plot

--- mcserver.py
+++ mcserver.py
@@ -1,4 +1,4 @@
-import ConfigParser, logging, datetime, os
+import ConfigParser, logging, datetime, os, json
 
 from flask import Flask, render_template, request
 
@@ -29,13 +29,33 @@ def home():
 @app.route("/search",methods=['POST'])
 def search_results():
     keywords = request.form['keywords']
-    now = datetime.datetime.now()
+    start_date = request.form['start_date']
+    end_date = request.form['end_date']
+
+    # now = datetime.datetime.now()
     results = mc.sentenceCount(keywords,
-                               solr_filter=[mc.publish_date_query( datetime.date( 2017, 1, 1), now ),
-                                            'tags_id_media:9139487'])
+                                solr_filter=[mc.publish_date_query(datetime.datetime.strptime(start_date, "%Y-%m-%d"),
+                                                                   datetime.datetime.strptime(end_date, "%Y-%m-%d")),
+                                            'tags_id_media:9139487'],
+                                split=True,
+                                split_start_date=start_date,
+                                split_end_date=end_date)
+    print(json.dumps(results['split'], indent=4, separators=(',', ': ')))
+    clean_data = {}
+    for key in results['split']:
+        # if a date, append to clean_data dict
+        if len(key.encode('utf-8')) > 5:
+            clean_data[key.encode('utf-8')] = results['split'][key]
+
+    # print(type(key.encode('utf-8')))
+    # print(json.dumps(clean_data))
+    # print(type(clean_data))
+    # print(type(json.dumps(clean_data)))
+    # print(type(json.loads(json.dumps(clean_data))))
     return render_template("search-results.html",
                            keywords=keywords,
-                           sentenceCount=results['count'] )
+                           sentenceCount=results['count'],
+                           weeklyResults=json.dumps(clean_data))
 
 if __name__ == "__main__":
     app.debug = True
